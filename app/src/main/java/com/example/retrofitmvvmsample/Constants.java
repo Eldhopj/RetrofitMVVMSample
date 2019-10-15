4f7281e15298200967f9446b702e45fdc7ef3470
eldhopj <eldho@live.in>
2019-10-15 19:19:21 +0530

    Initial setup

---
+++ app/src/main/java/com/example/retrofitmvvmsample/Constants.java
@@ -0,0 +1,5 @@
+package com.example.retrofitmvvmsample;
+
+public class Constants {
+    public static final boolean DEBUG_MODE = true;
+}
---
+++ app/src/main/java/com/example/retrofitmvvmsample/RetrofitClient.java
@@ -0,0 +1,42 @@
+package com.example.retrofitmvvmsample;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "https://reqres.in/";
+
+    private static RetrofitClient mInstance;
+    private final Retrofit retrofit;
+
+    public static synchronized RetrofitClient getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new RetrofitClient(context);
+        }
+        return mInstance;
+    }
+
+    private RetrofitClient(Context context) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        if (Constants.DEBUG_MODE) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.level(HttpLoggingInterceptor.Level.BODY);
+            httpClient.addInterceptor(logging);
+        }
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+
+
+}
